0393d3e585994103398e995345c973307f516efe
NaganandhiniNagendran <nandhucse2006@gmail.com>
2026-03-12 13:23:54 +0530

    Portfilo

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
   output: 'export',
-  trailingSlash: true,
-  distDir: 'out',
   basePath: '/Portfilo',
-  assetPrefix: '/Portfilo',
-}
+  assetPrefix: '/Portfilo/',
+};
 
 export default nextConfig
